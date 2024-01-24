973e2a6c7eb396e57de94c269643e32e8e59db57
exs777 <exs777@mail.ru>
2024-01-24 23:33:02 +0300

    commit2

--- src/main/java/web/controller/TableController.java
+++ src/main/java/web/controller/TableController.java
@@ -1,14 +1,10 @@
 package web.controller;
 
-import model.Car;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import service.TabService;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class TableController {
